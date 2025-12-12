b542dc179378bb1f73f4adc64f50c561a5c68c0c
Saurav Jain <sauravjain0503@gmail.com>
2025-12-12 15:35:50 +0530

    Updated the constants file and implemented mobile responsive fixes

---
+++ src/data/navLinks.js
@@ -0,0 +1,9 @@
+const navLinks = [
+  { label: 'Home', href: '/' },
+  { label: 'Bike Rental', href: '#' },
+  { label: 'Partner Login', href: '#' },
+  { label: 'Used Bike Valuation', href: '#' },
+  { label: 'RTO Agent Pune', href: '#' },
+];
+
+export default navLinks;
